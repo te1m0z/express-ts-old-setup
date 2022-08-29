26bf29584ee960656d044e0cfb9daec7fbe13b35
te1m0z <dmitriy.balamozhnov@gmail.com>
2022-08-29 23:36:25 +0400

    работа над стартом БД

--- database.js
+++ database.js
@@ -1,20 +1,40 @@
+require("dotenv").config();
 const sqlite3 = require("sqlite3").verbose();
 const bcrypt = require("bcrypt");
 
 const DBSOURCE = "./database/db.sqlite";
 
-let db = new sqlite3.Database(DBSOURCE, (err) => {
-    if (err) {
-        console.error(err.message);
-        throw err;
+const checkUsersTableExists = () => {
+    db.get("SELECT * FROM ?", null, (err, row) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+
+const databaseCallback = (createError) => {
+    if (createError) {
+        console.log("databaseCallback: ", createError.message); throw createError;
     } else {
-        const sql = `CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            login text UNIQUE NOT NULL,
-            password text NOT NULL
-        )`;
-        db.run(sql);
+        const sql = `
+            CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                login TEXT UNIQUE NOT NULL,
+                PASSWORD TEXT NOT NULL
+            )
+        `;
+
+        const bcryptHandle = (err, hash) => {
+            if (checkUsersTableExists) {
+            } else {
+                db.run(sql);
+            }
+        };
+
+        bcrypt.hash(process.env.DB_PASS, 10, bcryptHandle);
     }
-});
+};
+
+let db = new sqlite3.Database(DBSOURCE, databaseCallback);
 
 module.exports = db;
